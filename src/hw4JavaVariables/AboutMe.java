9e119a208e43bdd345a5ad991af607a9aa9562e0
Ifzal2023 <ifzal.mi@gmail.com>
2023-01-18 15:49:13 -0600

    Homewrok # 4

--- src/hw4JavaVariables/AboutMe.java
+++ src/hw4JavaVariables/AboutMe.java
@@ -1,10 +1,33 @@
 package hw4JavaVariables;
 
+/*
+ Create a package name "hw4JavaVariables" inside the HW project. Create a Class name "AboutMe". Declare String and other 8 types of variable. Create a constructor and Give a sysout inside Constructor which will print "This is all about us: ". Then Create a method name aboutMe. Inside the method -use String and the other 8 types of variables inside sysout to define a person's meaningful info.
+ */
+
 public class AboutMe {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-// 2nd push
+	// Below variables are declared
+	public String name;
+	public byte myAge;
+	public short myCarPayment;
+	public int myHouseNumber;
+	public long myBankBalance;
+	public float myHeight;
+	public double myGrade;
+	public char myShirtSize;
+	public boolean vietnamWarVeteran;
+
+	// Below Constructor is declared
+	public AboutMe() {
+		System.out.println("This is all about us: ");
+	}
+
+	// Below method is implemented
+	public void aboutMe() {
+		System.out.println("My Name: " + name + "\nMy Age: " + myAge + "\nMy Car Payment: " + myCarPayment
+				+ "\nMy House Number: "+ myHouseNumber + "\nMy Bank Balance" + myBankBalance + "\nMy Shirt Size: "
+				+ myShirtSize + "\nMy Height: " + myHeight + "\nMy Grade: " + myGrade
+				+ "\nAm I Vietnam War Veteran? Ans: " + vietnamWarVeteran);
 	}
 
 }
---
+++ src/hw4JavaVariables/AboutMeTest.java
@@ -0,0 +1,39 @@
+package hw4JavaVariables;
+/*
+ Create another class AboutMeTest. Instantiate AboutMe class inside the main method. Initialize variables and call the method by the object. I expect print outcome for 2 people, one for you and another for your best friend Alex. give a single line comment where a variable is declared and initialized, the constructor is declared and initialized, and the method is implemented and initialized.
+Follow indentation by Organizing the code and push to GitHub and paste the link of GitHub below. [Mark distribution (100): using variable --10, Create a method and constructor - 20, instantiate class - 10, initialize variables and call methods for you and Alex= 15  variable is declared and initialized, the constructor is declared and initialized, the method is implemented and initialized -- 10, organize code and other-- 25]. push your code to Git Hub -- 10. Share the package link below.
+ 
+ */
+
+public class AboutMeTest {
+	public static void main(String[] args) {
+		AboutMe ifzal = new AboutMe(); // // Constructor is initialized here
+		// variables are initialized below
+		ifzal.name = "Muhammad A Mughnee";
+		ifzal.myAge = 35;
+		ifzal.myCarPayment = 600;
+		ifzal.myHouseNumber = 3142;
+		ifzal.myBankBalance = 123456213l;
+		ifzal.myHeight = 5.5f;
+		ifzal.myGrade = 3.894562;
+		ifzal.myShirtSize = 'M';
+		ifzal.vietnamWarVeteran = false;
+		ifzal.aboutMe(); // Method is initialized here
+
+		System.out.println("\n-----------------------------------------------------------------------\n");
+
+		AboutMe alex = new AboutMe();
+		alex.name = "Alex Rodriguez";
+		alex.myAge = 55;
+		alex.myCarPayment = 900;
+		alex.myHouseNumber = 46523;
+		alex.myBankBalance = 2356983l;
+		alex.myHeight = 6.2f;
+		alex.myGrade = 3.4562;
+		alex.myShirtSize = 'L';
+		alex.vietnamWarVeteran = false;
+		alex.aboutMe(); // Method is initialized here
+
+	}
+
+}
