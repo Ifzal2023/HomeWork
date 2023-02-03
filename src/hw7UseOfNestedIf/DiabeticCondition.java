260d6f97251f61e2cd7f3eb793152f562b343cde
Ifzal2023 <ifzal.mi@gmail.com>
2023-02-03 12:21:10 -0600

    hw7UseOfNestedIf

--- src/hw7UseOfNestedIf/DiabeticCondition.java
+++ src/hw7UseOfNestedIf/DiabeticCondition.java
@@ -7,29 +7,26 @@ import java.util.Scanner;
  */
 
 public class DiabeticCondition {
+
 	public static void main(String[] args) {
 		System.out.println("<-- Please Enter your Hemoglobin A1C  value below -->");
-		
-		Scanner scanner = new Scanner(System.in); // 
-		double hbA1c = scanner.nextDouble(); // 
-		
+
+		Scanner scanner = new Scanner(System.in);
+		double hbA1c = scanner.nextDouble();
+
 		if (hbA1c > 6.4) {
-			System.out.println( " I am a diabetic patient");
-			
-			
-							
-			}	else	if (hbA1c >= 5.7) {
+			System.out.println(" I am a diabetic patient");
+
+		} else {
+			if (hbA1c >= 5.7) {
 				System.out.println(" I am a pre-diabetic patient");
-			} else (hbA1c <5.7) {
+			} else if (hbA1c < 5.7) {
 				System.out.println(" I am a healthy person");
-				
+
 			}
 			scanner.close();
-			
-				
-			}
+
+		}
 	}
-		
-	
 
 }
